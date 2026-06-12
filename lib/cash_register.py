8dffd3acac63e6a34348b4ab631f39c68148d84b
Yasin <yasinhassan004@gamil.com>
2026-06-12 15:06:29 +0300

    Add cash register class attributes

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+  
+
+from itertools import count
+
 
 class CashRegister:
+  def __init__(self, discount=0):
+    self.total = 0
+    self.items = []
+    self.previous_transactions = []
+    self.discount = discount 
+  
+    
   pass
